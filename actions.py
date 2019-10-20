c61bb181d4087989b686516b9f5f969f23f879bf
PrakharBhagat <prakharbhagat@gmail.com>
2019-10-20 11:29:18 +0530

    Adding more:  1. stories  2. Training data  3. Code optimization

--- actions.py
+++ actions.py
@@ -9,162 +9,166 @@ import mailsmyp
 import json
 import base64
 
+
 class ActionSearchRestaurants(Action):
-	def name(self):
-		return 'action_restaurant'
-		
-	def run(self, dispatcher, tracker, domain):
-		config={ "user_key":"889129b59670ab646cf410cadfc1886b"}
-		zomato = zomatopy.initialize_app(config)
-		loc = tracker.get_slot('location')
-		cuisine = tracker.get_slot('cuisine')
-		price = tracker.get_slot('price')
-		location_detail=zomato.get_location(loc, 1)
-		d1 = json.loads(location_detail)
-		lat=d1["location_suggestions"][0]["latitude"]
-		lon=d1["location_suggestions"][0]["longitude"]
-		city_name = str(d1["location_suggestions"][0]["city_name"]).lower()
-		cuisines_dict = {'mexican': 73, 'chinese': 25, 'american': 1, 'italian': 55, 'north indian': 50,
-						 'south indian': 85}
-		city_set = {"bengaluru", "chennai", "delhi", "hyderabad", "kolkata", "mumbai", "ahmedabad", "pune", "agra",
-					"ajmer", "aligarh", "amravati", "amritsar", "asansol", "aurangabad", "bareilly", "belgaum",
-					"bhavnagar", "bhiwandi", "bhopal", "bhubaneswar", "bikaner", "bilaspur", "bokarosteelcity",
-					"chandigarh", "coimbatorenagpur", "cuttack", "dehradun", "dhanbad", "bhilai", "durgapur", "erode",
-					"faridabad", "firozabad", "ghaziabad", "gorakhpur", "gulbarga", "guntur", "gwalior", "gurgaon",
-					"guwahati", "hubli–dharwad", "indore", "jabalpur", "jaipur", "jalandhar", "jammu", "jamnagar",
-					"jamshedpur", "jhansi", "jodhpur", "kakinada", "kannur", "kanpur", "kochi", "kottayam", "kolhapur",
-					"kollam", "kota", "kozhikode", "kurnool", "ludhiana", "lucknow", "madurai", "malappuram", "mathura",
-					"goa", "mangalore", "meerut", "moradabad", "mysore", "nanded", "nashik", "nellore", "noida",
-					"palakkad", "patna", "pondicherry", "puruliaallahabad", "raipur", "rajkot", "rajahmundry", "ranchi",
-					"rourkela", "salem", "sangli", "siliguri", "solapur", "srinagar", "thiruvananthapuram", "thrissur",
-					"tiruchirappalli", "tirupati", "tirunelveli", "tiruppur", "tiruvannamalai", "ujjain", "bijapur",
-					"vadodara", "varanasi", "vasai-virarcity", "vijayawada", "vellore", "warangal", "surat",
-					"visakhapatnam"}
+    def name(self):
+        return 'action_restaurant'
 
-		isCityServiceable = city_name in city_set
-		response = ""
-		if isCityServiceable and cuisine in cuisines_dict:
-			response = self.findTopFive(lat, lon, str(cuisines_dict.get(cuisine)), price, zomato)
-		elif not isCityServiceable:
-			response = "Sorry this city is not serviceable"
-		elif cuisine not in cuisines_dict:
-			response = "Sorry this cuisine is not available"
+    def run(self, dispatcher, tracker, domain):
+        response = ""
+        config = {"user_key": "c86b663aedfe8a2b54af34bfb337bb5c"}
+        zomato = zomatopy.initialize_app(config)
+        loc = tracker.get_slot('location')
+        cuisine = tracker.get_slot('cuisine')
+        price = tracker.get_slot('price')
+        location_detail = zomato.get_location(loc, 1)
+        d1 = json.loads(location_detail)
+        if len(d1["location_suggestions"]) == 0:
+            dispatcher.utter_message('We do not operate in that area yet.')
+            return [SlotSet('location', None)]
+        lat = d1["location_suggestions"][0]["latitude"]
+        lon = d1["location_suggestions"][0]["longitude"]
+        city_name = str(d1["location_suggestions"][0]["city_name"]).lower()
+        cuisines_dict = {'mexican': 73, 'chinese': 25, 'american': 1, 'italian': 55, 'north indian': 50,
+                         'south indian': 85}
+        city_set = {"bhubaneswar", "kolhapur", "mathura", "delhi ncr", "kanpur", "bhavnagar", "jammu", "delhi ncr",
+                    "amravati", "gorakhpur", "vijayawada", "bokaro", "amritsar", "trichy", "kottayam", "srinagar",
+                    "siliguri", "delhi ncr", "palakkad", "firozabad", "vadodara", "purulia", "trivandrum", "tirupati",
+                    "durgapur", "lucknow", "ranchi", "bijapur", "varanasi", "bhopal", "allahabad", "patna",
+                    "visakhapatnam", "dehradun", "nagpur", "mysore", "mumbai", "guntur", "dhanbad", "jhansi", "nanded",
+                    "goa", "indore", "rajahmundry", "mumbai", "tiruvannamalai", "ludhiana", "kollam", "solapur",
+                    "kozhikode", "vellore", "kurnool", "thrissur", "dharwad", "jalandhar", "meerut", "raipur",
+                    "durg bhilai", "cuttack", "chennai", "moradabad", "bilaspur", "tirunelveli", "kannur", "bengaluru",
+                    "kakinada", "surat", "belgaum", "hyderabad", "erode", "jabalpur", "pune", "rajkot", "agra",
+                    "mangalore", "delhi ncr", "aligarh", "kota", "bhiwadi", "malappuram", "salem", "ahmedabad",
+                    "nellore", "asansol", "warangal", "nashik", "coimbatore", "chennai", "aurangabad", "ajmer",
+                    "gulbarga", "madurai", "chandigarh", "delhi ncr", "sangli", "jaipur", "guwahati", "ujjain",
+                    "mumbai", "hubli", "bikaner", "bareilly", "kolkata", "jamshedpur", "rourkela", "jamnagar",
+                    "jodhpur", "kochi", "gwalior", "tiruppur"}
 
+        isCityServiceable = city_name in city_set
 
-		dispatcher.utter_message("-----"+response)
-		return [SlotSet('location',loc)]
+        if isCityServiceable and cuisine in cuisines_dict:
+            response = self.findTopFive(lat, lon, str(cuisines_dict.get(cuisine)), price, zomato)
+        elif not isCityServiceable:
+            dispatcher.utter_message("We do not operate in that area yet.")
+            return [SlotSet('location', None)]
 
-	def findTopFive(self, lat, lon, cusine, price, zomato):
-		counter = 0
-		offset = 0
-		response = ""
-		while True:
-			results = zomato.restaurant_search("", lat, lon, cusine, offset)
-			d = json.loads(results)
-			if "code" not in d:
-				if d['results_found'] == 0:
-					if counter == 0:
-						response = "Sorry, no results found"
-					break
-				if price == "300":
-					filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] <= 300]
-				elif price == "300 to 700":
-					filteredResult = [x for x in d['restaurants'] if
-									  x['restaurant']['average_cost_for_two'] >= 300 and x['restaurant'][
-										  'average_cost_for_two'] <= 700]
-				else:
-					filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] >= 700]
+        elif cuisine not in cuisines_dict:
+            dispatcher.utter_message("Sorry this cuisine is not available")
+            return [SlotSet('cuisine', None)]
 
-				for restaurant in filteredResult:
-					if counter == 5:
-						break
-					response = response + "[" + restaurant['restaurant']['name'] + "] in [" + \
-							   restaurant['restaurant']['location']['address'] + "] has been rated :" + \
-							   restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
-					counter = counter + 1
+        dispatcher.utter_message("-----\n" + response)
+        return [SlotSet('location', loc)]
 
 
-			if counter == 5:
-				break
-			if "code" in d:
-				if counter != 0:
-					break
-				else:
-					response = "Sorry, server busy try after sometime."
-			offset = offset + 20
-		return response
+    def findTopFive(self, lat, lon, cusine, price, zomato):
+        counter = 0
+        offset = 0
+        response = ""
+        while True:
+            results = zomato.restaurant_search("", lat, lon, cusine, offset)
+            d = json.loads(results)
+            if "code" not in d:
+                if d['results_found'] == 0:
+                    if counter == 0:
+                        response = "Sorry, no results found"
+                    break
+                if price == "300":
+                    filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] <= 300]
+                elif price == "300 to 700":
+                    filteredResult = [x for x in d['restaurants'] if
+                                      x['restaurant']['average_cost_for_two'] >= 300 and x['restaurant'][
+                                          'average_cost_for_two'] <= 700]
+                else:
+                    filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] >= 700]
+
+                for restaurant in filteredResult:
+                    if counter == 5:
+                        break
+                    response = response + "[" + restaurant['restaurant']['name'] + "] in [" + \
+                               restaurant['restaurant']['location']['address'] + "] has been rated :" + \
+                               restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
+                    counter = counter + 1
+
+            if counter == 5:
+                break
+            if "code" in d:
+                if counter != 0:
+                    break
+                else:
+                    return "Sorry, server busy try after sometime."
+            offset = offset + 20
+        return response
+
 
 class ActionSendMail(Action):
-	def name(self):
-		return 'action_mail'
+    def name(self):
+        return 'action_mail'
 
-	def run(self, dispatcher, tracker, domain):
-		configZomato = {"user_key": "889129b59670ab646cf410cadfc1886b"}
-		zomato = zomatopy.initialize_app(configZomato)
-		loc = tracker.get_slot('location')
-		cuisine = tracker.get_slot('cuisine')
-		price = tracker.get_slot('price')
-		location_detail = zomato.get_location(loc, 1)
-		d1 = json.loads(location_detail)
-		lat = d1["location_suggestions"][0]["latitude"]
-		lon = d1["location_suggestions"][0]["longitude"]
-		cuisines_dict = {'mexican': 73, 'chinese': 25, 'american': 1, 'italian': 55, 'north indian': 50,
-						 'south indian': 85}
+    def run(self, dispatcher, tracker, domain):
+        configZomato = {"user_key": "c86b663aedfe8a2b54af34bfb337bb5c"}
+        zomato = zomatopy.initialize_app(configZomato)
+        loc = tracker.get_slot('location')
+        cuisine = tracker.get_slot('cuisine')
+        price = tracker.get_slot('price')
+        location_detail = zomato.get_location(loc, 1)
+        d1 = json.loads(location_detail)
+        lat = d1["location_suggestions"][0]["latitude"]
+        lon = d1["location_suggestions"][0]["longitude"]
+        cuisines_dict = {'mexican': 73, 'chinese': 25, 'american': 1, 'italian': 55, 'north indian': 50,
+                         'south indian': 85}
 
-		if cuisine in cuisines_dict:
-			response = self.findTopTen(lat, lon, str(cuisines_dict.get(cuisine)), price, zomato)
-		elif cuisine not in cuisines_dict:
-			response = "Sorry this cuisine is not available"
+        if cuisine in cuisines_dict:
+            response = self.findTopTen(lat, lon, str(cuisines_dict.get(cuisine)), price, zomato)
+        elif cuisine not in cuisines_dict:
+            response = "Sorry this cuisine is not available"
 
+        config = {"user_mail": "kumarprakharbhagat.ml7@iiitb.net", "user_password": "Prakhar@1989"}
+        mail = mailsmyp.initialize_app(config)
+        to = tracker.get_slot('emailid')
+        mail.send_mail(to, response)
 
-		config = {"user_mail": "kumarprakharbhagat.ml7@iiitb.net", "user_password": "Prakhar@1989"}
-		mail = mailsmyp.initialize_app(config)
-		to = tracker.get_slot('emailid')
-		mail.send_mail(to, response)
+    def findTopTen(self, lat, lon, cusine, price, zomato):
+        counter = 0
+        offset = 0
+        response = "Hi There," + "\n\n" + "Please find the requested restaurant details" + "\n\n\n"
+        while True:
+            results = zomato.restaurant_search("", lat, lon, cusine, offset)
+            d = json.loads(results)
+            if "code" not in d:
+                if d['results_found'] == 0:
+                    if counter == 0:
+                        response = "Sorry, no results found"
+                    break
+                if price == "300":
+                    filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] <= 300]
+                elif price == "300 to 700":
+                    filteredResult = [x for x in d['restaurants'] if
+                                      x['restaurant']['average_cost_for_two'] >= 300 and x['restaurant'][
+                                          'average_cost_for_two'] <= 700]
+                else:
+                    filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] >= 700]
 
-	def findTopTen(self, lat, lon, cusine, price, zomato):
-		counter = 0
-		offset = 0
-		response = "Hi There," + "\n\n" + "Please find the requested restaurant details" + "\n\n\n"
-		while True:
-			results = zomato.restaurant_search("", lat, lon, cusine, offset)
-			d = json.loads(results)
-			if "code" not in d:
-				if d['results_found'] == 0:
-					if counter == 0:
-						response = "Sorry, no results found"
-					break
-				if price == "300":
-					filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] <= 300]
-				elif price == "300 to 700":
-					filteredResult = [x for x in d['restaurants'] if
-									  x['restaurant']['average_cost_for_two'] >= 300 and x['restaurant'][
-										  'average_cost_for_two'] <= 700]
-				else:
-					filteredResult = [x for x in d['restaurants'] if x['restaurant']['average_cost_for_two'] >= 700]
+                for restaurant in filteredResult:
+                    if counter == 10:
+                        break
+                    response = response + "Name: " + restaurant['restaurant']['name'] + "\nAddress: " + \
+                               restaurant['restaurant']['location']['address'] + "\nRating: " + \
+                               restaurant['restaurant']['user_rating']['aggregate_rating'] + \
+                               "\nCost for two here is Rs." + str(
+                        restaurant['restaurant']['average_cost_for_two']) + "\n\n"
+                    counter = counter + 1
 
-				for restaurant in filteredResult:
-					if counter == 10:
-						break
-					response = response + "[" + restaurant['restaurant']['name'] + "] in [" + \
-							   restaurant['restaurant']['location']['address'] + "] has been rated :" + \
-							   restaurant['restaurant']['user_rating']['aggregate_rating'] + \
-							   ", avg cost for two here is Rs." + str(
-						restaurant['restaurant']['average_cost_for_two']) + "\n"
-					counter = counter + 1
+            if counter == 10:
+                break
+            if "code" in d:
+                if counter != 0:
+                    break
+                else:
+                    return "Sorry, server busy try after sometime."
+            offset = offset + 20
 
-			if counter == 10:
-				break
-			if "code" in d:
-				if counter != 0:
-					break
-				else:
-					response = "Sorry, server busy try after sometime."
-			offset = offset + 20
+        response = response + "\n\n" + "Best regards," + "\n" + "Foodie Inc."
 
-		response = response + "\n\n" + "Best regards," + "\n" + "Foodie Inc."
-
-		return response
-
-
-
+        return response
--- data/stories.md
+++ data/stories.md
@@ -1,46 +1,83 @@
-## Generated Story 255706069223404498
+## Generated Story 3880746886605172270
 * greet
     - utter_greet
-* restaurant_search
+* restaurant_search{"cuisine": "north indian"}
+    - slot{"cuisine": "north indian"}
     - utter_ask_location
-* restaurant_search{"location": "delhi"}
-    - slot{"location": "delhi"}
-    - utter_ask_cuisine
-* restaurant_search{"cuisine": "chinese"}
-    - slot{"cuisine": "chinese"}
+* restaurant_search{"location": "bangalore"}
+    - slot{"location": "bangalore"}
+    - utter_ask_price
+* restaurant_search{"price": "700"}
+    - slot{"price": "700"}
     - action_restaurant
-    - slot{"location": "delhi"}
-    - utter_goodbye
-    - export
-
-## Generated Story 1993277579540566202
-* greet
-    - utter_greet
-* restaurant_search
-    - utter_ask_location
-* restaurant_search{"location": "delhi"}
-    - slot{"location": "delhi"}
-    - utter_ask_cuisine
-* restaurant_search{"cuisine": "North Indian"}
-    - slot{"cuisine": "North Indian"}
-    - action_restaurant
-    - utter_goodbye
-
-## Generated Story 3320800183399695936
-* greet
-    - utter_greet
-* restaurant_search
-    - utter_ask_location
-* restaurant_search{"location": "italy"}
-    - slot{"location": "italy"}
-	- utter_ask_cuisine
-* restaurant_search{"cuisine": "chinese"}
-    - slot{"cuisine": "chinese"}
-    - action_restaurant
+    - slot{"location": "bangalore"}
+    - utter_ask_for_mail
 * goodbye
     - utter_goodbye
+    - export
 
-## Generated Story -4639179087166749998
+## Generated Story -7273996254737092498
+* greet
+    - utter_greet
+* restaurant_search{"location": "bangalore"}
+    - slot{"location": "bangalore"}
+    - utter_ask_cuisine
+* restaurant_search{"cuisine": "italian"}
+    - slot{"cuisine": "italian"}
+* restaurant_search{"price": "300 to 700"}
+    - slot{"price": "300 to 700"}    
+    - action_restaurant
+    - slot{"location": "bangalore"}
+    - utter_ask_for_mail
+* affirm
+    - utter_ask_mailid
+* restaurant_search{"emailid": "belagali.pooja@gmail.com"}
+    - slot{"emailid": "belagali.pooja@gmail.com"}
+    - action_mail
+    - utter_goodbye
+    - export
+
+## Generated Story 4815117106456005560
+* greet
+    - utter_greet
+* restaurant_search{"location": "chennai"}
+    - slot{"location": "chennai"}
+    - utter_ask_cuisine
+* restaurant_search{"cuisine": "american"}
+    - slot{"cuisine": "american"}
+    - utter_ask_price
+* restaurant_search{"price": "300 to 700"}
+    - slot{"price": "300 to 700"}
+    - action_restaurant
+    - slot{"location": "chennai"}
+    - utter_ask_for_mail
+* affirm
+    - utter_ask_mailid
+* restaurant_search{"emailid": "belagali.pooja@gmail.com"}
+    - slot{"emailid": "belagali.pooja@gmail.com"}
+    - action_mail
+    - utter_goodbye
+    - export
+
+## Generated Story 7592444740733743432
+* greet
+    - utter_greet
+* restaurant_search{"cuisine": "american"}
+    - slot{"cuisine": "american"}
+    - utter_ask_location
+* restaurant_search{"location": "kolkata"}
+    - slot{"location": "kolkata"}
+    - utter_ask_price
+* restaurant_search{"price": "700"}
+    - slot{"price": "700"}
+    - action_restaurant
+    - slot{"location": "kolkata"}
+    - utter_ask_for_mail
+* goodbye
+    - utter_goodbye
+    - export
+
+## Generated Story -3087864445675830362
 * greet
     - utter_greet
 * restaurant_search
@@ -48,22 +85,146 @@
 * restaurant_search{"location": "delhi"}
     - slot{"location": "delhi"}
     - utter_ask_cuisine
-* restaurant_search{"cuisine": "chinese"}
-    - slot{"cuisine": "chinese"}
+* restaurant_search{"cuisine": "south indian"}
+    - slot{"cuisine": "south indian"}
+    - utter_ask_price
+* restaurant_search{"price": "300"}
+    - slot{"price": "300"}
     - action_restaurant
     - slot{"location": "delhi"}
-    - export
-
-
-## Generated Story 4963448062290237512
-* greet
-    - utter_greet
-* restaurant_search{"location": "delhi"}
-    - slot{"location": "delhi"}
-    - utter_ask_cuisine
-* restaurant_search{"cuisine": "chinese"}
-    - slot{"cuisine": "chinese"}
-    - action_restaurant
-* affirm
+    - utter_ask_for_mail
+* goodbye
     - utter_goodbye
     - export
+
+## Generated Story -5869706125876783594
+* greet
+    - utter_greet
+* restaurant_search{"location": "noida"}
+    - slot{"location": "noida"}
+    - utter_ask_cuisine
+* restaurant_search{"cuisine": "north indian"}
+    - slot{"cuisine": "north indian"}
+    - utter_ask_price
+* restaurant_search{"price": "300"}
+    - slot{"price": "300"}
+    - action_restaurant
+    - slot{"location": "noida"}
+    - utter_ask_for_mail
+* affirm
+    - utter_ask_mailid
+* restaurant_search{"emailid": "prakharbhagat@gmail.com"}
+    - slot{"emailid": "prakharbhagat@gmail.com"}
+    - action_mail
+    - utter_goodbye
+    - export
+
+
+## Generated Story 564735842531408083
+* greet
+    - utter_greet
+* restaurant_search
+    - utter_ask_location
+* restaurant_search{"location": "pune"}
+    - slot{"location": "pune"}
+    - utter_ask_cuisine
+* restaurant_search{"cuisine": "mexican"}
+    - slot{"cuisine": "mexican"}
+    - utter_ask_price
+* restaurant_search{"price": "300 to 700"}
+    - slot{"price": "300 to 700"}
+    - action_restaurant
+    - slot{"location": "pune"}
+    - utter_ask_for_mail
+* goodbye
+    - utter_goodbye
+    - export
+
+## Generated Story 1166232535489337169
+* greet
+    - utter_greet
+* restaurant_search{"location": "nasik"}
+    - slot{"location": "nasik"}
+    - utter_ask_cuisine
+* restaurant_search{"cuisine": "north indian"}
+    - slot{"cuisine": "north indian"}
+    - utter_ask_price
+* restaurant_search{"price": "300"}
+    - slot{"price": "300"}
+    - action_restaurant
+    - slot{"location": "nasik"}
+    - utter_ask_for_mail
+* goodbye
+    - utter_goodbye
+    - export
+
+## Generated Story 5566224104215452549
+* greet
+    - utter_greet
+* restaurant_search{"cuisine": "mexican", "location": "kolkata"}
+    - slot{"cuisine": "mexican"}
+    - slot{"location": "kolkata"}
+    - utter_ask_price
+* restaurant_search{"price": "700"}
+    - slot{"price": "700"}
+    - action_restaurant
+    - slot{"location": "kolkata"}
+    - utter_ask_for_mail
+* affirm
+    - utter_ask_mailid
+* restaurant_search{"emailid": "prakharbhagat@gmail.com"}
+    - slot{"emailid": "prakharbhagat@gmail.com"}
+    - action_mail
+    - utter_goodbye
+    - export
+
+## Generated Story -7279536267822436613
+* greet
+    - utter_greet
+* restaurant_search{"cuisine": "mexican", "location": "bhagalpur"}
+    - slot{"cuisine": "mexican"}
+    - slot{"location": "bhagalpur"}
+    - utter_ask_price
+* restaurant_search{"price": "700"}
+    - slot{"price": "700"}
+    - action_restaurant
+    - slot{"location": null}
+    - utter_goodbye
+    - export
+
+## Generated Story -1690811419344999220
+* greet
+    - utter_greet
+* restaurant_search{"cuisine": "south indian", "price": "300"}
+    - slot{"cuisine": "south indian"}
+    - slot{"price": "300"}
+    - utter_ask_location
+* restaurant_search{"location": "belgaum"}
+    - slot{"location": "belgaum"}
+    - action_restaurant
+    - slot{"location": "belgaum"}
+    - utter_ask_for_mail
+* affirm
+    - utter_ask_mailid
+* restaurant_search{"emailid": "belagali.pooja@gmail.com"}
+    - slot{"emailid": "belagali.pooja@gmail.com"}
+    - action_mail
+    - utter_goodbye
+    - export
+
+## Generated Story 5404477888299937548
+* greet
+    - utter_greet
+* restaurant_search{"cuisine": "mexican", "price": "300 to 700"}
+    - slot{"cuisine": "mexican"}
+    - slot{"price": "300 to 700"}
+    - utter_ask_location
+* restaurant_search{"location": "bangalore"}
+    - slot{"location": "bangalore"}
+    - action_restaurant
+    - slot{"location": "bangalore"}
+    - utter_ask_for_mail
+* goodbye
+    - utter_goodbye
+    - export
+
